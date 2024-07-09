edacc3fb8cfa7b314515d05e255371700f370147
geralt <geralt@pop.com>
2024-07-09 12:22:35 -0400

    simple example working

---
+++ code/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gfortran -o ./ftorch_example ./main.f90 -I/usr/local/ftorch/include/ftorch -L/usr/local/ftorch/lib -lftorch
+
+#export LD_LIBRARY_PATH=/usr/local/ftorch/lib:$LD_LIBRARY_PATH
